ae91724a94de78de7e4140cd297920bf82bf8553
Leung <charlottexprojects@gmail.com>
2021-06-24 16:55:31 +0800

    add the search page and button

--- app/src/main/java/com/charlotteprojects/androidminiproject/MainActivity.java
+++ app/src/main/java/com/charlotteprojects/androidminiproject/MainActivity.java
@@ -2,13 +2,33 @@ package com.charlotteprojects.androidminiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //region init the layout
+
+        Button ButtonStart = findViewById(R.id.main_button_start);
+
+        //endregion
+
+        //region init the Button Onclick
+
+        ButtonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchPage.class);
+                startActivity(intent);
+            }
+        });
+
+        //endregion
     }
 }
