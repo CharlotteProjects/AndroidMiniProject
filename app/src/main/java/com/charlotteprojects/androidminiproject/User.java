4d0d9836557bcbe8d878ca743a1f264676f481df
Leung <charlottexprojects@gmail.com>
2021-07-17 21:12:53 +0800

    add manage page

--- app/src/main/java/com/charlotteprojects/androidminiproject/User.java
+++ app/src/main/java/com/charlotteprojects/androidminiproject/User.java
@@ -1,14 +1,22 @@
 package com.charlotteprojects.androidminiproject;
 
 public class User {
-    public String userName, userEmail, shopName;
+    public String userName, userEmail, shopName, latitude, longitude;
 
     public User() {
+
     }
 
     public User(String name, String email, String shop){
         userName = name;
         userEmail = email;
         shopName = shop;
+        latitude = "";
+        longitude = "";
+    }
+
+    public void SetAddress(String addLatitude, String addLongitude){
+        latitude = addLatitude;
+        longitude = addLongitude;
     }
 }
